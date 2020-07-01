d92a24dcf14b310f1fe7159816c6ea9492f2c1df
adrian_visan1998 <visanadrian1998@gmail.com>
2020-07-01 11:56:38 +0300

    logic for add and remove buttons

--- popup.js
+++ popup.js
@@ -5,21 +5,21 @@ let changeableEmojis = ["✪", "⚔️", "☎", "♛", "♫", "☯", "❤️"];
 const runApp = (tab) => {
   //CHECK IF TINDER PAGE IS OPEN IN TAB
   if (tab.url.includes("tinder.com")) {
-    tabId = tab.id;
+    const tabId = tab.id;
     //selectElements();
     //updatePopup();
   } else {
-    addAndRemoveButtons = document.getElementById("addAndRemoveButtons");
-    notOnTinder = document.getElementById("notOnTinder");
+    const addAndRemoveButtons = document.getElementById("addAndRemoveButtons");
+    const notOnTinder = document.getElementById("notOnTinder");
+
+    //IF WE ARE NOT ON TINDER -> DISPLAY NOTONTINDER COMPONENT
     addAndRemoveButtons.style.display = "none";
     notOnTinder.style.display = "inline";
-    tinderLink = document.getElementById("openTinder");
-    //tinderLink.addEventListener("click", function () {
-    //chrome.tabs.create({ url: "https://tinder.com", active: true });
-    //});
-    tinderLink.onClick = () => {
+
+    const tinderLink = document.getElementById("openTinder");
+    tinderLink.addEventListener("click", function () {
       chrome.tabs.create({ url: "https://tinder.com", active: true });
-    };
+    });
   }
 };
 
@@ -29,3 +29,20 @@ window.addEventListener("DOMContentLoaded", () =>
     (tabs) => tabs.length && runApp(tabs[0])
   )
 );
+
+const selectElements = () => {
+  const addMessageButton = document.getElementById("addMessage");
+  const removeAllMessagesButton = document.getElementById("removeAllButtons");
+
+  removeAllMessagesButton.addEventListener("click", function () {
+    chrome.tabs.sendMessage(tabId, { remove: "remove" });
+    chrome.storage.local.set({ emojis: predefinedemojis }, function () {});
+    window.close();
+  });
+
+  addMessageButton.addEventListener("click", function () {
+    const messagetext = document.createElement("input");
+    messagetext.type = "text";
+    addMessageButton.insertAdjacentElement("beforebegin", messagetext);
+  });
+};
