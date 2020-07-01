94fbf3142da7acbae3f7292de0b39964c6fbeb6c
adrian_visan1998 <visanadrian1998@gmail.com>
2020-07-01 11:11:20 +0300

    updated manifest page and added few components

---
+++ popup.js
@@ -0,0 +1,28 @@
+let tabId = null;
+const stockEmojis = ["✪", "⚔️", "☎", "♛", "♫", "☯", "❤️"];
+let changeableEmojis = ["✪", "⚔️", "☎", "♛", "♫", "☯", "❤️"];
+
+const runApp = (tab) => {
+  //CHECK IF TINDER PAGE IS OPEN IN TAB
+  if (tab.url.includes("tinder.com")) {
+    tabId = tab.id;
+    selectElements();
+    updatePopup();
+  } else {
+    addAndRemoveButtons = document.getElementById("messageList");
+    notOnTinder = document.getElementById("not-tinder");
+    messageList.style.display = "none";
+    notOnTinder.style.display = "inline";
+  }
+};
+
+window.addEventListener("DOMContentLoaded", () =>
+  chrome.tabs.query(
+    { active: true, currentWindow: true },
+    (tabs) => tabs.length && runApp(tabs[0])
+  )
+);
+
+notOnTinder.onClick = () => {
+  chrome.tabs.create({ url: "https://tinder.com", active: true });
+};
